8576844f533b33119fb61d8afc621770f3c6afae
Harish A Kulkarni <harishakulkarni@gmail.com>
2023-02-10 21:19:36 +0530

    Product of two numbers input from iostream

---
+++ Chapter1/Exercise1_4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+   int a = 0, b = 0;
+   std::cin >> a >> b;
+   std::cout << "Product of the numbers " 
+   << a << " and " << b << " is " << a * b << std::endl;  //endl inserts new line character into output stream and flushes it.
+}
