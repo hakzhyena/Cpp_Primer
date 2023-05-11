5e287fdbba5cea8e8c83426309417109af1d9cca
Harish A Kulkarni <harishakulkarni@gmail.com>
2023-05-11 20:31:10 +0530

    A Separate using is Required for Each Name

---
+++ Chapter3/getSpecificNamesFromNamespace.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cin; 
+using std::cout;
+using std::endl;
+
+int main()
+{
+   cout << "Enter two numbers " << endl;
+   int i{}, j{};
+   cin >> i >> j;
+   cout << "Sum of the entered number is: " << i + j << endl;
+   return 0;
+}
